a340a94e036ac5b2f6b7e5141b7391d466194afd
lidankatzav <lidankatzav5@gmail.com>
2022-12-29 14:55:56 +0200

    update server ex2

---
+++ chat-server/src/services/messages/index.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from 'express';
+import addUsersNamesToMsgs from './convertMessages';
+
+export const messages = Router();
+
+messages.get('/',(req: Request, res: Response) => {
+    const messagesListWithNames = addUsersNamesToMsgs();
+    res.send(messagesListWithNames);
+});
+
+export default messages;
+
+
+
+
