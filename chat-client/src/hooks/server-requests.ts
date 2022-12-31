9eb2b1be2b7bdae9c0a81b1b2ffc07e67c61f557
lidankatzav <lidankatzav5@gmail.com>
2022-12-31 03:04:33 +0200

    update

--- chat-client/src/hooks/server-requests.ts
+++ chat-client/src/hooks/server-requests.ts
@@ -1,7 +1,7 @@
 import {Message} from '../types/message';
 import {User} from '../types/user';
 
-const url: string = 'http://localhost:3000';
+const url: string = 'http://localhost:3005';
 
 /**
  * GET Request to get the list of messages
